f5fb7aef296c416d08dc6fa0caf03b3d4097621b
James Almond <james@jamesalmond.com>
2012-05-14 10:51:54 +0100

    Added sample spec file

---
+++ spec/golf_spec.rb
@@ -0,0 +1,21 @@
+describe "Golf" do
+  describe ".hole1", :hole => 1 do
+  end
+  describe ".hole2", :hole => 2 do
+  end
+  describe ".hole3", :hole => 3 do
+  end
+  describe ".hole4", :hole => 4 do
+  end
+  describe ".hole5", :hole => 5 do
+  end
+  describe ".hole6", :hole => 6 do
+  end
+  describe ".hole7", :hole => 7 do
+  end
+  describe ".hole8", :hole => 8 do
+  end
+  describe ".hole9", :hole => 9 do
+  end
+end
+
