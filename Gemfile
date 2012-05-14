5da36552882be114c9fc1a2143fc5e6602f3aebe
James Almond <james@jamesalmond.com>
2012-05-14 10:52:30 +0100

    Pusher functionality

--- Gemfile
+++ Gemfile
@@ -3,3 +3,4 @@ source "http://rubygems.org"
 gem 'rake'
 gem 'rspec', '~> 2.10'
 gem 'guard-rspec'
+gem 'pusher'
--- spec/golf_formatter.rb
+++ spec/golf_formatter.rb
@@ -1,5 +1,8 @@
 require "rspec/core/formatters/base_text_formatter"
 require File.dirname(__FILE__) + '/line_counter.rb'
+require 'pusher'
+require 'yaml'
+
 class GolfFormatter < RSpec::Core::Formatters::BaseTextFormatter
 
   def initialize(output)
@@ -7,6 +10,12 @@ class GolfFormatter < RSpec::Core::Formatters::BaseTextFormatter
     @passed_examples = []
     @passed_holes = []
     @failed_holes = []
+
+    begin
+      @pusher_config = YAML.load_file(File.dirname(__FILE__) + "/../pusher.yml")
+    rescue Exception => e
+      puts e
+    end
   end
 
   def example_passed(example)
@@ -27,8 +36,33 @@ class GolfFormatter < RSpec::Core::Formatters::BaseTextFormatter
 
   def close
     super
-    puts "Hole in one: #{@passed_holes.map{|h| h.metadata[:hole]}.join(', ')}" unless @passed_holes.empty?
-    puts "In the rough: #{@failed_holes.map{|h| h.metadata[:hole]}.join(', ')}"
-    puts "Character Count: #{LineCounter.count(File.dirname(__FILE__) + "/../lib/golf.rb")}"
+    passed_nos = @passed_holes.map{|h| h.metadata[:hole]}
+    failed_nos = @failed_holes.map{|h| h.metadata[:hole]}
+    character_count = LineCounter.count(File.dirname(__FILE__) + "/../lib/golf.rb")
+
+    print_scorecard(passed_nos, failed_nos, character_count)
+    push(passed_nos, failed_nos, character_count)
+
   end
+
+  private
+
+    def print_scorecard(passed_nos, failed_nos, character_count)
+      puts "Hole in one: #{passed_nos.join(', ')}" unless passed_nos.empty?
+      puts "In the rough: #{failed_nos.join(', ')}"
+      puts "Character Count: #{character_count}"
+    end
+
+    def push(passed_nos, failed_nos, character_count)
+      Pusher.app_id = @pusher_config["pusher_app_id"]
+      Pusher.key = @pusher_config["pusher_key"]
+      Pusher.secret = @pusher_config["pusher_secret"]
+
+      begin
+        Pusher['golf'].trigger!('spec_run', {:my_name => @pusher_config["my_name"], :passed => passed_nos, :failed => failed_nos, :character_count => character_count})
+      rescue Pusher::Error => e
+        puts "Failed to send stats to server"
+      end
+    end
+
 end
